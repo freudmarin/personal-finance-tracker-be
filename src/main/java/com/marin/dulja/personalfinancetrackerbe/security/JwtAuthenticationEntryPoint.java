6fefd17bdde8b498a241aaf96727c1067e719848
marin.dulja <duljamarin@gmail.com>
2025-12-11 07:52:15 +0100

    Added cors headers for 401 responses

--- src/main/java/com/marin/dulja/personalfinancetrackerbe/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/marin/dulja/personalfinancetrackerbe/security/JwtAuthenticationEntryPoint.java
@@ -16,6 +16,13 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
+        String origin = request.getHeader("Origin");
+        if (origin != null) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
